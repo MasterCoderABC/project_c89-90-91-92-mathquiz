c469c2fa76ab02de0e89d3ad43e43d0e105f1b9d
AahanIsHere <97660379+MasterCoderABC@users.noreply.github.com>
2022-10-12 18:09:25 +0530

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,9 @@
+function addUser(){
+   var player1_name = document.getElementById("player1_name_input").value;
+   var player2_name = document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name", player2_name);
+
+    window.location.replace("quiz_game_page.html");
+}
