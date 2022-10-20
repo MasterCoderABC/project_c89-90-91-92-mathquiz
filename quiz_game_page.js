4667a90143f4d7f7b5b91e85e425cf28ef55d341
AahanIsHere <97660379+MasterCoderABC@users.noreply.github.com>
2022-10-20 11:09:34 +0530

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -13,10 +13,10 @@ document.getElementById("player2_score").innerHTML = player2_score;
 document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
 document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
 
+var actualAnswer;
 function send(){
     var number1 = document.getElementById("number1").value;
     var number2 = document.getElementById("number2").value;
-    var actualAnswer;
     var randomNumber = Math.floor(Math.random() * 4) + 1;
     var question_number;
 
@@ -38,7 +38,124 @@ function send(){
     var row = question_number + input_box + check_button;
     document.getElementById("output").innerHTML = row;
     
-    document.getElementById("number1").value = "";
-    document.getElementById("number2").value = "";
+    document.getElementById("number1").style.visibility = "hidden";
+    document.getElementById("number2").style.visibility = "hidden";
+    document.getElementById("button").style.visibility = "hidden";
+    document.getElementById("label1").style.visibility = "hidden";
+    document.getElementById("label2").style.visibility = "hidden";
+}
 
+var questionTurn = "player1";
+var answerTurn = "player2";
+var questionTurns = 0;
+var round = 1;
+
+function check(){
+   var announce = document.getElementById("announce");
+   var getAnswer = document.getElementById("input_check_box").value;
+   if(getAnswer == actualAnswer){
+    if (answerTurn == "player1"){
+        player1_score = player1_score + 1;
+        document.getElementById("player1_score").innerHTML = player1_score;
+        announce.innerHTML = "Point to: " + player1_name + "!"
+     }
+     else{
+        player2_score = player2_score + 1;
+        document.getElementById("player2_score").innerHTML = player2_score;
+        announce.innerHTML = "Point to: " + player2_name + "!"
+     }
+   }else{
+    if (answerTurn == "player1"){
+       player2_score = player2_score + 1;
+       document.getElementById("player2_score").innerHTML = player2_score;
+       announce.innerHTML = "Point to: " + player2_name + "!"
+    }
+    else{
+       player1_score = player1_score + 1;
+       document.getElementById("player1_score").innerHTML = player1_score;
+       announce.innerHTML = "Point to: " + player1_name + "!"
+    }
+ }
+ if (questionTurn == "player1"){
+    questionTurn = "player2";
+    document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name;
+    questionTurns = questionTurns + 1;
+    answerTurn = "player1";
+    document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name;
+ }
+ else{
+    questionTurn = "player1";
+    document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+    questionTurns = questionTurns + 1;
+    answerTurn = "player2";
+    document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
+ }
+ 
+
+ document.getElementById("output").innerHTML = "";
+ document.getElementById("number1").value = "";
+ document.getElementById("number2").value = "";
+ document.getElementById("label1").style.visibility = "visible";
+ document.getElementById("label2").style.visibility = "visible";
+ document.getElementById("number1").style.visibility = "visible";
+ document.getElementById("number2").style.visibility = "visible";
+ document.getElementById("button").style.visibility = "visible";
+
+ if (questionTurns == 2){
+    round = 2;
+    document.getElementById("round").innerHTML = "Round 2 of 10";
+ }else if(questionTurns == 4){
+    round = 3;
+    document.getElementById("round").innerHTML = "Round 3 of 10";
+ }else if(questionTurns == 6){
+    round = 4;
+    document.getElementById("round").innerHTML = "Round 4 of 10";
+ }else if(questionTurns == 8){
+    round = 5;
+    document.getElementById("round").innerHTML = "Round 5 of 10";
+ }else if(questionTurns == 10){
+    round = 6;
+    document.getElementById("round").innerHTML = "Round 6 of 10";
+ }else if(questionTurns == 12){
+    round = 7;
+    document.getElementById("round").innerHTML = "Round 7 of 10";
+ }else if(questionTurns == 14){
+    round = 8;
+    document.getElementById("round").innerHTML = "Round 8 of 10";
+ }else if(questionTurns == 16){
+    round = 9;
+    document.getElementById("round").innerHTML = "Round 9 of 10";
+ }else if(questionTurns == 18){
+    round = 10;
+    document.getElementById("round").innerHTML = "Round 10 of 10";
+ }else if(questionTurns == 20){
+    if(player1_score > player2_score){
+       announce.innerHTML = "Player 1 wins!";
+       document.getElementById("number1").style.visibility = "hidden";
+       document.getElementById("number2").style.visibility = "hidden";
+       document.getElementById("button").style.visibility = "hidden";
+       document.getElementById("label1").style.visibility = "hidden";
+       document.getElementById("label2").style.visibility = "hidden";
+       document.getElementById("player_question").style.visibility = "hidden";
+       document.getElementById("player_answer").style.visibility = "hidden";
+    }else if(player1_score == player2_score){
+       announce.innerHTML = "It's a tie!";
+       document.getElementById("number1").style.visibility = "hidden";
+       document.getElementById("number2").style.visibility = "hidden";
+       document.getElementById("button").style.visibility = "hidden";
+       document.getElementById("label1").style.visibility = "hidden";
+       document.getElementById("label2").style.visibility = "hidden";
+       document.getElementById("player_question").style.visibility = "hidden";
+       document.getElementById("player_answer").style.visibility = "hidden";
+    }else if(player1_score < player2_score){
+       announce.innerHTML = "Player 2 wins!";
+       document.getElementById("number1").style.visibility = "hidden";
+       document.getElementById("number2").style.visibility = "hidden";
+       document.getElementById("button").style.visibility = "hidden";
+       document.getElementById("label1").style.visibility = "hidden";
+       document.getElementById("label2").style.visibility = "hidden";
+       document.getElementById("player_question").style.visibility = "hidden";
+       document.getElementById("player_answer").style.visibility = "hidden";
+    }
+ }
 }
